93f6b98fc30dc5c735cc348dbf6a8d22014b005d
Pansyuk <panasyuk20anna@gmail.com>
2018-07-12 12:42:12 +0300

    added scripts

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 $(function(){
-    $(".language .dropbtn").on('click', function() {
-        if(!$(this).closest('.language').find('.dropdown-content').hasClass('dropdown-content-active')) {
-            $(this).closest('.language').find('.dropdown-content').addClass('dropdown-content-active');
-        } else {
-            $(this).closest('.language').find('.dropdown-content').removeClass('dropdown-content-active');
-        }
-    });
-});
-
-$(function(){
     $(".profile .dropbtn").on('click', function() {
         if(!$(this).closest('.profile').find('.dropdown-content').hasClass('dropdown-content-active')) {
             $(this).closest('.profile').find('.dropdown-content').addClass('dropdown-content-active');
         } else {
             $(this).closest('.profile').find('.dropdown-content').removeClass('dropdown-content-active');
+        }
+    });
+});
+$(function(){
+    $(document).click(function (e){
+        var div = $(".profile");
+        if ($('.profile .dropdown-content').hasClass("dropdown-content-active")) {
+            if ( !div.is(e.target) && div.has(e.target).length === 0 ) {
+                $('.profile .dropdown-content').removeClass('dropdown-content-active');
+            }
         }
     });
 });
